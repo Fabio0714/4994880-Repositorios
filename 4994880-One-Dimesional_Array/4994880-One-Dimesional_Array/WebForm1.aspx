d2b6d07017fa83084a4b04c5bfaab6ffa492bf67
unknown <fabose.herampos.617@gmail.com>
2025-03-06 11:08:54 -0600

    Primercommit

---
+++ 4994880-One-Dimesional_Array/4994880-One-Dimesional_Array/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="_4994880_One_Dimesional_Array.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:Label runat="server" Font-Bold="true" Text="Create Category Array"></asp:Label>
+
+            <br /><br />
+             
+            <asp:Button runat="server" ID="btnCreateArray" Text="Create Array" OnClick="btnCreateArray_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ 4994880-One-Dimesional_Array/4994880-One-Dimesional_Array/WebForm1.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _4994880_One_Dimesional_Array
+{
+	public partial class WebForm1 : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+        protected void btnCreateArray_Click(object sender, EventArgs e)
+        {
+			//Declare and load the array
+
+			String[] strCategoryArray = new string[5];
+			strCategoryArray[0] = "Footwear - Men's";
+			strCategoryArray[1] = "CLothings - Men's";
+			strCategoryArray[2] = "Raquets";
+			strCategoryArray[3] = "Footwear - Women's";
+			strCategoryArray[4] = "Clothing - Women's";
+
+			//Save the arrat in a session variable for future use
+			Session["strcategoryArray"] = strCategoryArray;
+
+			//Goto the next page
+
+			Response.Redirect("PageArray.aspx");
+		}
+    }
+}
